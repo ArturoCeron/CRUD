511608e39e8234d185496330b1a469a95a70666c
Arturo Ceron <43083944+ArturoCeron@users.noreply.github.com>
2021-06-25 16:44:53 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT ||  process.env.BIND_IP,
     db: process.env.MONGODB || 'mongodb+srv://proyectoCRUD:Lx3ydN3Fs0LTIJF4@cluster0.6zsec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     urlParser : {
         useNewUrlParser: true,
